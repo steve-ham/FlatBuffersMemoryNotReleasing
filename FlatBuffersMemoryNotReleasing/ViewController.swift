8d9d3a98657b25376c60d5add795428d66ac881f
steve.ham <steve.ham@kakaomobility.com>
2025-07-18 21:19:36 +0900

    Initial Commit

---
+++ FlatBuffersMemoryNotReleasing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlatBuffersMemoryNotReleasing
+//
+//  Created by steve.ham on 7/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
